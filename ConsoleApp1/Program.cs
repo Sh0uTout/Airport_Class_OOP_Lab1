a0d022d94fce9be63e5de956d768262961c13179
Sh0uTout <ebonitesh0ut@gmail.com>
2021-09-13 20:23:31 +0300

    In progress 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Airportement
+    {
+
+        class Airport
+        {
+            public string name;
+            public string status;
+            protected uint _totalPassangers;
+            protected uint _totalPlanes;
+            protected List<Plane> Planes = new List<Plane>();
+
+            public Airport()
+            {
+                Console.WriteLine("Enter name of the airport.");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter current status of work of the airport. (working / closed)");
+                status = Console.ReadLine();
+            }
+
+            public Airport(string __name, string __status)
+            {
+                name = __name;
+                status = __status;
+            }
+            
+            
+            
+        }
+
+
+        class Plane : Airport
+        {
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
